f311fb0d60fdd03c40efa27d9cb3b1604787e7ef
orange <clinlee.me@gmail.com>
2016-12-21 15:47:15 +0800

    修复重复扫描的bug

--- biu.py
+++ biu.py
@@ -17,8 +17,10 @@ import defusedxml.cElementTree as ET
 
 today = str(datetime.datetime.today()).split(' ')[0].replace('-', '.')
 
+targets = []
+
+
 def parse_crossdomain(target):
-    target = target.strip('\n').strip('\t').strip(' ')
     url = 'http://{}/crossdomain.xml'.format(target)
     response = requests.get(url, timeout=1).text
     try:
@@ -28,34 +30,50 @@ def parse_crossdomain(target):
             elems = dom.findall(".//allow-access-from")
             for el in elems:
                 target = el.get('domain').replace('*', 'www')
-                scan(target)
+                generate_url(target)
     except:
         pass
 
 
-def scan(target):
+def generate_url(target):
+    if target in targets:
+        return
+    else:
+        targets.append(target)
     for plugin in plugins:
+        urls = []
         with open(plugin) as f:
             plugin = json.load(f)
-        url = 'http://{}{}'.format(target, plugin['suffix'])
-        try:
-            if plugin['method'] in ['GET']:
-                http = requests.get
+        if plugin['port'] == [80]:
+            urls.append('http://{}{}'.format(target, plugin['suffix']))
+        else:
+            for port in plugin['port']:
+                urls.append('http://{}:{}{}'.format(target, port, plugin[
+                    'suffix']))
+        for url in urls:
+            audit(url, plugin)
+
+
+def audit(url, plugin):
+    try:
+        if plugin['method'] in ['GET']:
+            http = requests.get
+        else:
+            http = requests.post
+        # response = http(url, timeout=1,verify=False).text
+        response = http(url, timeout=1).text
+        for hit in plugin['hits']:
+            if hit not in response:
+                pass
             else:
-                http = requests.post
-            # response = http(url, timeout=1,verify=False).text
-            response = http(url, timeout=1).text
-            for hit in plugin['hits']:
-                if hit not in response:
-                    pass
-                else:
-                    print('\033[0;92m[+]{}\t[{}]'.format(url, plugin['name']))
-                    with open('reports/result_{}_{}.txt'.format(plugin['name'], today), 'a+')as res:
-                        res.writelines(url + '\n')
-                    break
-                print('\033[0;31m[-] \033[0;29m{}'.format(url))
-        except:
-            pass
+                print('\033[0;92m[+]{}\t[{}]'.format(url, plugin['name']))
+                with open('reports/result_{}_{}.txt'.format(
+                        plugin['name'], today), 'a+') as result_file:
+                    result_file.writelines(url + '\n')
+                break
+            print('\033[0;31m[-] \033[0;29m{}'.format(url))
+    except:
+        pass
 
 
 if __name__ == '__main__':
@@ -68,19 +86,19 @@ if __name__ == '__main__':
     p = Pool(10)
     if args.f:
         domain_file = args.f
-        with open(domain_file, 'r')as f:
+        with open(domain_file, 'r') as f:
             for target in f.readlines():
                 target = target.strip('\n').strip('\t').strip(' ')
-                p.apply_async(scan, (target,))
-                p.apply_async(parse_crossdomain, (str(target),))
+                p.apply_async(generate_url, (target, ))
+                p.apply_async(parse_crossdomain, (target, ))
         p.close()
         p.join()
     elif args.d:
-        scan(args.d)
+        generate_url(args.d)
     elif args.a:
         for target in ipaddress.IPv4Network(args.a):
-            p.apply_async(scan, (str(target),))
-            p.apply_async(parse_crossdomain, (str(target),))
+            p.apply_async(generate_url, (str(target), ))
+            p.apply_async(parse_crossdomain, (str(target), ))
         p.close()
         p.join()
     else:
